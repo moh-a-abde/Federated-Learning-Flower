17855189e5aa3b3035bc397f2c50929959b7f1de
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-06-23 17:16:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def main(cfg: DictConfig):
         num_clients=cfg.num_clients,
         config=fl.server.ServerConfig(num_rounds=cfg.num_rounds),
         strategy=strategy,
-        client_resources={'num_cpus': 4},
+        
 
     )
 
