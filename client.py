7bafdf9a9f8d9f6d0920e07e59fc12ba78848070
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-06-24 19:04:25 -0400

    Update client.py

--- client.py
+++ client.py
@@ -4,7 +4,6 @@ from flwr.common import NDArrays, Scalar
 import torch
 import flwr as fl
 import numpy as np
-import os
 
 from model import XGBoostModel, train_xgboost_model, test_xgboost_model
 
@@ -16,7 +15,7 @@ class FlowerClient(fl.client.NumPyClient):
         self.valloader = valloader
         self.num_classes = num_classes
         self.input_dim = input_dim
-        self.model_path = f"model_{os.getpid()}.xgb"
+        self.model = None
 
     def set_parameters(self, parameters):
         # This can be left empty as XGBoost does not need this method
@@ -31,15 +30,14 @@ class FlowerClient(fl.client.NumPyClient):
         X_train, y_train = self._prepare_data(self.trainloader)
         
         # Train the XGBoost model
-        self.model = train_xgboost_model(X_train, y_train, self.num_classes, self.input_dim, save_path=self.model_path)
+        self.model = train_xgboost_model(X_train, y_train, self.num_classes, self.input_dim)
 
         return self.get_parameters({}), len(self.trainloader), {}
 
     def evaluate(self, parameters: NDArrays, config: Dict[str, Scalar]):
         X_val, y_val = self._prepare_data(self.valloader)
-        if os.path.exists(self.model_path):
-            self.model = XGBoostModel(self.num_classes, self.input_dim)
-            self.model.model = xgb.Booster(model_file=self.model_path)
+        if self.model is None:
+            raise ValueError("Model is not trained yet")
         accuracy, report = test_xgboost_model(self.model, X_val, y_val)
         loss = 1 - accuracy  # Mock loss as XGBoost does not provide a direct loss value
         return float(loss), len(self.valloader), {'accuracy': accuracy}
