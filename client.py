aaebe5a400160626b8b879262a3597cd7830bd96
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-06-25 16:09:25 -0400

    Update client.py

--- client.py
+++ client.py
@@ -3,9 +3,7 @@ from typing import Dict
 from flwr.common import NDArrays, Scalar
 import torch
 import flwr as fl
-import torch.optim as optim
 import xgboost as xgb
-import pickle
 
 from model import Net, train, test
 
@@ -18,24 +16,21 @@ class FlowerClient(fl.client.NumPyClient):
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     def set_parameters(self, parameters):
-        # Deserialize the parameters (assuming they are stored in a binary format)
-        self.model.model = xgb.Booster()
+        if self.model.model is None:
+            self.model.model = xgb.Booster()
         self.model.model.load_model(parameters)
 
     def get_parameters(self, config: Dict[str, Scalar]):
         if self.model.model is None:
             raise ValueError("Model is not initialized or trained yet.")
-        # Serialize the model parameters
         return self.model.model.save_raw()
 
     def fit(self, parameters, config):
-        # Set the parameters
         self.set_parameters(parameters)
         lr = config['lr']
         momentum = config['momentum']
         epochs = config['local_epochs']
-        # Dummy optimizer (not used in XGBoost)
-        optim = None
+        optim = None  # Dummy optimizer
         train(self.model, self.trainloader, optim, epochs, self.device)
         return self.get_parameters({}), len(self.trainloader), {}
 
