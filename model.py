d6c0d15895bdd5db6e47d2bf3041877c0a1ed10f
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-06-24 22:06:16 -0400

    Update model.py

--- model.py
+++ model.py
@@ -1,35 +1,42 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
 
 class Net(nn.Module):
     def __init__(self, num_classes: int, input_dim: int) -> None:
         super(Net, self).__init__()
+        #self.conv1 = nn.Conv2d(1, 6, 5)
+        #self.pool = nn.MaxPool2d(2, 2)
+        #self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(input_dim, 128)
         self.fc2 = nn.Linear(128, 64)
         self.fc3 = nn.Linear(64, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        #x = self.pool(F.relu(self.conv1(x)))
+        #x = self.pool(F.relu(self.conv2(x)))
+        #x = x.view(-1, 16 * 4 * 4)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
-
-def train(net, trainloader, optimizer, scheduler, epochs, device: str):
+def train(net, trainloader, optimizer, epochs, device: str):
+        
+    # Train network on training set
     criterion = torch.nn.CrossEntropyLoss()
     net.train()
     net.to(device)
-    for epoch in range(epochs):
+    for _ in range(epochs):
         for features, labels in trainloader:
             features, labels = features.to(device), labels.to(device)
-            optimizer.Adam()
+            optimizer.zero_grad()
             loss = criterion(net(features), labels)
             loss.backward()
             optimizer.step()
-        scheduler.step()
 
 def test(net, testloader, device: str):
+    #  Validate network on entire test set
+
     criterion = torch.nn.CrossEntropyLoss()
     correct, loss = 0, 0.0
     net.eval()
