5a337bf0c1acec0309bf617c4e4fffac78ef688f
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-06-26 07:15:20 -0400

    Update model.py

--- model.py
+++ model.py
@@ -1,59 +1,46 @@
 import xgboost as xgb
-from sklearn.metrics import accuracy_score
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from typing import Tuple
 
-import xgboost as xgb
-import numpy as np
-
-class Net:
+class XGBoostModel:
     def __init__(self, num_classes: int, input_dim: int) -> None:
         self.num_classes = num_classes
         self.input_dim = input_dim
-        self.model = None
-        self.params = {
-            'max_depth': 6,
-            'eta': 0.3,
-            'objective': 'multi:softprob',
-            'num_class': num_classes
-        }
+        self.model = xgb.XGBRegressor(
+            n_estimators=100,
+            max_depth=3,
+            learning_rate=0.1,
+            objective='reg:squarederror' if num_classes == 1 else 'multi:softprob',
+            num_class=num_classes if num_classes > 1 else None,
+            random_state=42
+        )
+    
+    def forward(self, X: np.ndarray) -> np.ndarray:
+        return self.model.predict(X)
 
-    def get_model_bytes(self):
-        if self.model is None:
-            return None
-        return self.model.save_raw()
+def train(model: XGBoostModel, X_train: np.ndarray, y_train: np.ndarray, epochs: int, device: str = 'cpu'):
+    # XGBoost doesn't use GPU by default, so we ignore the device parameter
+    # We'll use the number of epochs as n_estimators
+    model.model.n_estimators = epochs
+    model.model.fit(
+        X_train, 
+        y_train, 
+        eval_set=[(X_train, y_train)], 
+        early_stopping_rounds=10,
+        verbose=True
+    )
 
-    def set_model_bytes(self, model_bytes):
-        if model_bytes is not None:
-            self.model = xgb.Booster()
-            self.model.load_model(bytearray(model_bytes))
-        else:
-            self.model = None
-
-    def train_model(self, trainloader, epochs):
-        train_features, train_labels = self._loader_to_numpy(trainloader)
-        dtrain = xgb.DMatrix(train_features, label=train_labels)
-        
-        if self.model is None:
-            self.model = xgb.train(self.params, dtrain, num_boost_round=epochs)
-        else:
-            self.model = xgb.train(self.params, dtrain, num_boost_round=epochs, xgb_model=self.model)
-
-    def _loader_to_numpy(self, loader):
-        features, labels = [], []
-        for data, label in loader:
-            features.append(data.numpy())
-            labels.append(label.numpy())
-        return np.vstack(features), np.hstack(labels)
+def test(model: XGBoostModel, X_test: np.ndarray, y_test: np.ndarray, device: str = 'cpu') -> Tuple[float, float]:
+    # Again, we ignore the device parameter for XGBoost
+    y_pred = model.forward(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    accuracy = model.model.score(X_test, y_test)
+    return mse, accuracy
 
 def early_stopping(val_losses, patience=10):
     if len(val_losses) > patience:
         if all(val_losses[-1] > val_losses[-(i+2)] for i in range(patience)):
             return True
     return False
-
-# Standalone functions to be imported
-def train(net, trainloader, optimizer, epochs, device: str):
-    net.train_model(trainloader, optimizer, epochs, device)
-
-def test(net, testloader, device: str):
-    return net.test_model(testloader, device)
