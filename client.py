37c8e288a879ca8ab1f3e45713a542d9706d07e3
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-06-28 16:10:32 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,12 +1,17 @@
 from collections import OrderedDict
 from typing import Dict
-import flwr as fl
 from flwr.common import NDArrays, Scalar
+import torch
+import flwr as fl
+import torch.optim as optim
 
 from model import Net, train, test
 
 class FlowerClient(fl.client.NumPyClient):
-    def __init__(self, trainloader, valloader, num_classes, input_dim) -> None:
+    def __init__(self,
+                 trainloader,
+                 valloader,
+                 num_classes, input_dim) -> None:
         super().__init__()
 
         self.trainloader = trainloader
@@ -14,47 +19,59 @@ class FlowerClient(fl.client.NumPyClient):
 
         self.model = Net(num_classes, input_dim)
 
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
     def set_parameters(self, parameters):
-        # Save the parameters to a temporary file and load them into the model
-        temp_model_path = 'temp_model.json'
-        with open(temp_model_path, 'wb') as f:
-            f.write(parameters[0])
-        self.model.load_model(temp_model_path)
+
+        params_dict = zip(self.model.state_dict().keys(), parameters)
+
+        state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
+
+        self.model.load_state_dict(state_dict, strict=True)
+    
     
     def get_parameters(self, config: Dict[str, Scalar]):
-        # Save the model parameters to a temporary file and return the file contents
-        temp_model_path = 'temp_model.json'
-        self.model.save_model(temp_model_path)
-        with open(temp_model_path, 'rb') as f:
-            parameters = [f.read()]
-        return parameters
+        
+        return [ val.cpu().numpy() for _, val in self.model.state_dict().items()]
+
     
     def fit(self, parameters, config):
-        # Copy parameters sent by the server into client's local model
+
+        # copy parameters sent by the server into client's local model
         self.set_parameters(parameters)
 
         lr = config['lr']
         momentum = config['momentum']
         epochs = config['local_epochs']
+        # Define the optimizer (e.g., Adam)
         
-        # No need to define optimizer for XGBoost
+        optim = torch.optim.SGD(self.model.parameters(), lr=lr, momentum=momentum)
 
-        # Do local training
-        train(self.model, self.trainloader, None, epochs, "cpu")
+        # do local training
+        train(self.model, self.trainloader, optim, epochs, self.device)
 
         return self.get_parameters({}), len(self.trainloader), {}
     
+    
     def evaluate(self, parameters: NDArrays, config: Dict[str, Scalar]):
+        
         self.set_parameters(parameters)
 
-        loss, accuracy = test(self.model, self.valloader, "cpu")
+        loss, accuarcy = test(self.model, self.valloader, self.device)
         
-        return float(loss), len(self.valloader), {'accuracy': accuracy}
+        return float(loss), len(self.valloader), {'accuarcy': accuarcy}
+    
+
 
 def generate_client_fn(trainloaders, valloaders, num_classes, input_dim):
+
     def client_fn(cid: str):
+
         return FlowerClient(trainloader=trainloaders[int(cid)],
                             valloader=valloaders[int(cid)],
                             num_classes=num_classes,
                             input_dim=input_dim)
+
+
     return client_fn
