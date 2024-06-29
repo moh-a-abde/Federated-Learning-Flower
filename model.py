e844d0fa918fa0f9b64ec4a1b3abb6ec3095e804
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-06-29 14:34:19 -0400

    Update model.py

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 import time
 
 class Net(nn.Module):
