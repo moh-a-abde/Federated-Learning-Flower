a420498cee445511872a598b3f49590e56efab6b
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-06-30 16:10:01 -0400

    Update model.py

--- model.py
+++ model.py
@@ -112,7 +112,7 @@ def train_xgboost():
         'max_depth': 6,
         'eta': 0.35,
         'objective': 'multi:softmax',
-        'num_class': 4,
+        'num_class': l,
         'eval_metric': 'merror',
         'tree_method': 'hist'
     }
