8dd8e65e787072b489280f6efa08033ee0ada568
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-07-01 20:55:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from pprint import pprint
 from dataset import prepare_dataset
 from client import generate_client_fn
 from server import get_on_fit_config, get_evaluate_fn
-from model import train_xgboost, Net, train_nn
+from model import train_xgboost
 
 @hydra.main(config_path="conf", config_name="base", version_base=None)
 def main(cfg: DictConfig):
@@ -23,15 +23,18 @@ def main(cfg: DictConfig):
                                                                   cfg.batch_size, num_classes=cfg.num_classes)
     # Check trainloaders, validationloaders, testloader
     print(f"Train loaders: {len(trainloaders)}, Validation loaders: {len(validationloaders)}, Test loader: {len(testloader)}, Input dim: {input_dim}'")
-    
     # 3. Define your clients
-    client_fn = generate_client_fn(trainloaders, validationloaders, testloader, cfg.num_classes, input_dim, cfg.model_types)
+    client_fn = generate_client_fn(trainloaders, validationloaders, testloader, cfg.num_classes, input_dim)
     
     # 4. Define your strategy
+
+    # Define the evaluation metrics aggregation function
     def evaluate_metrics_aggregation_fn(metrics):
-        accuracies = [metric[1]['accuracy'] for metric in metrics]
-        aggregated_accuracy = sum(accuracies) / len(accuracies)
-        return {'accuracy': aggregated_accuracy}
+        # Example: Aggregate accuarcy across all clients
+        accuarcies = [metric[1]['accuarcy'] for metric in metrics]
+        aggregated_accuarcy = sum(accuarcies) / len(accuarcies)
+        
+        return {'accuarcy': aggregated_accuarcy}
     
     strategy = fl.server.strategy.FedAvg(fraction_fit=0.00001,
                                          min_fit_clients=cfg.num_clients_per_round_fit,
@@ -39,7 +42,7 @@ def main(cfg: DictConfig):
                                          min_evaluate_clients=cfg.num_clients_per_round_eval,
                                          min_available_clients=cfg.num_clients,
                                          on_fit_config_fn=get_on_fit_config(cfg.config_fit),
-                                         evaluate_fn=get_evaluate_fn(cfg.num_classes, input_dim, testloader, cfg.model_types),
+                                         evaluate_fn=get_evaluate_fn(cfg.num_classes, input_dim, testloader),
                                          evaluate_metrics_aggregation_fn=evaluate_metrics_aggregation_fn)
 
     # 5. Start simulation
@@ -49,24 +52,21 @@ def main(cfg: DictConfig):
         config=fl.server.ServerConfig(num_rounds=cfg.num_rounds),
         strategy=strategy,
         client_resources={'num_cpus': 4},
+
     )
 
     # 6. Save results
     save_path = HydraConfig.get().runtime.output_dir
     results_path = Path(save_path) / 'results.pkl'
 
-    # 7. Train XGBoost classifier if specified in config
-    if 'xgb' in cfg.model_types:
-        xgboost_model = train_xgboost()
+    # 7. Train XGBoost classifier
+    xgboost_model = train_xgboost()
 
-    # Save models and history as required
-    results = {
-        'history': history,
-        'xgboost_model': xgboost_model if 'xgb' in cfg.model_types else None
-    }
-
-    with open(results_path, 'wb') as f:
-        pickle.dump(results, f)
+    #results = {
+    #    'history': history,
+    #    'nn_results': nn_results,
+    #    'xgboost_model': xgboost_model  # Save the model or its relevant metrics
+    #}
 
 if __name__ == "__main__":
     main()
