bbf6f485c377970f8ab4a292c52011f8d7a495a0
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-07-01 20:29:01 -0400

    Update client.py

--- client.py
+++ client.py
@@ -30,33 +30,21 @@ class FlowerClient(fl.client.NumPyClient):
             self.model.load_state_dict(state_dict, strict=True)
 
     def get_parameters(self, config: Dict[str, Scalar]) -> List[NDArrays]:
-        if self.model_type == 'nn':
-            return [val.cpu().numpy() for _, val in self.model.state_dict().items()]
-        elif self.model_type == 'xgb':
-            booster = self.model.get_booster()
-            return booster.save_raw().decode("utf-8")
+        return [val.cpu().numpy() for _, val in self.model.state_dict().items()]
 
     def fit(self, parameters: List[NDArrays], config: Dict[str, Scalar]):
-        if self.model_type == 'nn':
-            self.set_parameters(parameters)
-            lr = config['lr']
-            momentum = config['momentum']
-            epochs = config['local_epochs']
-            optimizer = torch.optim.SGD(self.model.parameters(), lr=lr, momentum=momentum)
-            train_nn(self.model, self.trainloader, self.testloader, optimizer, epochs, self.device)
-            return self.get_parameters({}), len(self.trainloader), {}
-        elif self.model_type == 'xgb':
-            self.model = train_xgboost()
-            return self.get_parameters({}), {}
+        self.set_parameters(parameters)
+        lr = config['lr']
+        momentum = config['momentum']
+        epochs = config['local_epochs']
+        optimizer = torch.optim.SGD(self.model.parameters(), lr=lr, momentum=momentum)
+        train_nn(self.model, self.trainloader, self.testloader, optimizer, epochs, self.device)
+        return self.get_parameters({}), len(self.trainloader), {}
 
     def evaluate(self, parameters: NDArrays, config: Dict[str, Scalar]):
-        if self.model_type == 'nn':
-            self.set_parameters(parameters)
-            loss, accuracy = test_nn(self.model, self.valloader, self.device)
-            return float(loss), len(self.valloader), {'accuracy': accuracy}
-        elif self.model_type == 'xgb':
-            accuracy = test_xgboost(self.model, self.valloader)
-            return 0.0, len(self.valloader), {'accuracy': accuracy}
+        self.set_parameters(parameters)
+        loss, accuracy = test_nn(self.model, self.valloader, self.device)
+        return float(loss), len(self.valloader), {'accuracy': accuracy}
 
 def generate_client_fn(trainloaders, valloaders, testloader, num_classes, input_dim, model_types):
     def client_fn(cid: str):
