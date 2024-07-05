2afb18cbb0de4f0de92cbe1a4b083f723855499a
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-07-05 16:17:24 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,49 +1,35 @@
-import flwr as fl
+from collections import OrderedDict
+from typing import Dict
+from flwr.common import NDArrays, Scalar
 import pandas as pd
+import flwr as fl
 import model
 
 class FlowerClient(fl.client.NumPyClient):
-    def __init__(self, model):
-        self.model = model
+    def __init__(self, trainloader, valloader, testloader):
+        super().__init__()
+        self.trainloader = trainloader
+        self.valloader = valloader
+        self.testloader = testloader
+        self.model = None
 
-    def get_parameters(self):
-        # Return model parameters as a list of NumPy ndarrays
-        return self.model.get_params()
+    def set_parameters(self, parameters):
+        pass
+
+    def get_parameters(self, config: Dict[str, Scalar]):
+        pass
 
     def fit(self, parameters, config):
-        # Set model parameters
-        self.model.set_params(parameters)
-
-        # Load the dataset
-        file_path = 'data/zeek_live_data_merged.csv'
-        data = pd.read_csv(file_path)
-
-        # Train the model
         self.model = model.train_xgboost()
+        return [], len(self.trainloader), {}
 
-        # Return updated model parameters and number of training examples
-        return self.model.get_params(), len(data), {}
+    def evaluate(self, parameters: NDArrays, config: Dict[str, Scalar]):
+        accuracy = 0.99  # Placeholder for actual accuracy
+        return 0.0, len(self.valloader), {'accuracy': accuracy}
 
-    def evaluate(self, parameters, config):
-        # Set model parameters
-        self.model.set_params(parameters)
-
-        # Load the dataset
-        file_path = 'data/zeek_live_data_merged.csv'
-        data = pd.read_csv(file_path)
-
-        # Evaluate the model
-        accuracy = model.evaluate_xgboost(self.model, data)
-
-        # Return loss, accuracy, and number of evaluation examples
-        return 0.0, accuracy, len(data)
-
-def main():
-    # Load the model
-    model_instance = model.train_xgboost()
-
-    # Start Flower client
-    fl.client.start_client(server_address="localhost:8080", client=FlowerClient(model_instance).to_client())
-
-if __name__ == "__main__":
-    main()
+def generate_client_fn(trainloaders, valloaders, testloader):
+    def client_fn(cid: str):
+        return FlowerClient(trainloader=trainloaders[int(cid)],
+                            valloader=valloaders[int(cid)],
+                            testloader=testloader)
+    return client_fn
