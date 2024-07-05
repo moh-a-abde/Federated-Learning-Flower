ec9827a01962bdcef77ccefc8e3da62bd4403311
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-07-05 16:08:38 -0400

    Update model.py

--- model.py
+++ model.py
@@ -1,89 +1,11 @@
 import pandas as pd
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import xgboost as xgb
-from sklearn.model_selection import train_test_split, StratifiedKFold
-from sklearn.metrics import accuracy_score, classification_report
 from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
 import time
 
-class Net(nn.Module):
-    def __init__(self, num_classes: int, input_dim: int) -> None:
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(input_dim, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 128)
-        self.fc4 = nn.Linear(128, 64)
-        self.fc5 = nn.Linear(64, num_classes)
-        self.dropout = nn.Dropout(0.5)
-    
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = F.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = F.relu(self.fc2(x))
-        x = self.dropout(x)
-        x = F.relu(self.fc3(x))
-        x = self.dropout(x)
-        x = F.relu(self.fc4(x))
-        x = self.fc5(x)
-        return x
-
-def train_nn(net, trainloader, testloader, optimizer, epochs, device: str):
-    print('Training Neural Network:')
-    criterion = torch.nn.CrossEntropyLoss()
-    net.train()
-    net.to(device)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.01, patience=2, verbose=True)
-    val_losses = []
-    for epoch in range(epochs):
-        running_loss = 0.0
-        for features, labels in trainloader:
-            features, labels = features.to(device), labels.to(device)
-            optimizer.zero_grad()
-            outputs = net(features)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            torch.nn.utils.clip_grad_norm_(net.parameters(), max_norm=1.0)
-            optimizer.step()
-            running_loss += loss.item()
-        epoch_loss = running_loss / len(trainloader)
-        print(f"Epoch {epoch+1}/{epochs}, Loss: {epoch_loss:.4f}")
-        scheduler.step(epoch_loss)
-        val_losses.append(epoch_loss)
-        if early_stopping(val_losses):
-            print("Early stopping triggered")
-            break
-        # Evaluate on the test set
-        test_loss, test_accuracy = test_nn(net, testloader, device)
-        print(f"Test Loss: {test_loss:.4f}, Test Accuracy: {test_accuracy:.4f}")
-
-
-def test_nn(net, testloader, device: str):
-    print('Testing Neural Network:')
-    criterion = torch.nn.CrossEntropyLoss()
-    correct, total_loss = 0, 0.0
-    net.eval()
-    net.to(device)
-    with torch.no_grad():
-        for features, labels in testloader:
-            features, labels = features.to(device), labels.to(device)
-            outputs = net(features)
-            total_loss += criterion(outputs, labels).item()
-            _, predicted = torch.max(outputs.data, 1)
-            correct += (predicted == labels).sum().item()
-    accuracy = correct / len(testloader.dataset)
-    avg_loss = total_loss / len(testloader)
-    return avg_loss, accuracy
-
-def early_stopping(val_losses, patience=10):
-    if len(val_losses) > patience:
-        if all(val_losses[-1] > val_losses[-(i+2)] for i in range(patience)):
-            return True
-    return False
-
 def train_xgboost():
-    
     # Load the dataset
     file_path = 'data/zeek_live_data_merged.csv'
     data = pd.read_csv(file_path)
@@ -144,7 +66,7 @@ def train_xgboost():
     print(f"Training Time: {TrainTime} seconds")
     print(f"Testing Time: {TestTime} seconds")
     accuracy = accuracy_score(y_train, predictions)
-    report = classification_report(y_train, predictions)
+    report = classification_report(y_train, predictions, zero_division=1)
     print('XGBoost Model Training Metrics:')
     print(f'Accuracy: {accuracy}')
     print('Classification Report:')
