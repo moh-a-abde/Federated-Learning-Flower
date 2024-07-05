f7418cb57e5879f69251779eed94fc62e5db183c
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-07-05 16:17:46 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,27 @@
+from collections import OrderedDict
 import flwr as fl
+from model import train_xgboost
 
-def start_server():
-    # Start Flower server
-    fl.server.start_server(server_address="0.0.0.0:8080", config={"num_rounds": 3})
+def get_on_fit_config():
+    def fit_config_fn(server_round: int):
+        return {'lr': 0.01, 'momentum': 0.99, 'local_epochs': 10}
+    return fit_config_fn
+
+def get_evaluate_fn():
+    def evaluate_fn(server_round: int, parameters, config):
+        model = train_xgboost()
+        accuracy = 0.99  # Placeholder for actual accuracy
+        return 0.0, {'accuracy': accuracy}
+    return evaluate_fn
 
 if __name__ == "__main__":
-    start_server()
+    strategy = fl.server.strategy.FedAvg(
+        fraction_fit=0.1,
+        fraction_eval=0.1,
+        min_fit_clients=2,
+        min_eval_clients=2,
+        min_available_clients=2,
+        eval_fn=get_evaluate_fn(),
+        on_fit_config_fn=get_on_fit_config(),
+    )
+    fl.server.start_server(config={"num_rounds": 10}, strategy=strategy)
