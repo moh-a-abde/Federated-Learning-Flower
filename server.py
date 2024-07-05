112446e4c87aca16c006806de571c442e9a048e3
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-07-05 15:52:52 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,33 +1,8 @@
-from collections import OrderedDict
+import flwr as fl
 
-from omegaconf import DictConfig
+def start_server():
+    # Start Flower server
+    fl.server.start_server(config={"num_rounds": 3})
 
-import torch
-
-from model import Net, test_nn
-
-def get_on_fit_config(config: DictConfig):
-    def fit_config_fn(server_round: int):
-        
-        return {'lr': config.lr, 'momentum': config.momentum,
-                'local_epochs': config.local_epochs}
-    
-    return fit_config_fn
-
-def get_evaluate_fn(num_classes: int, input_dim: int, testloader):
-
-
-    def evaluate_fn(server_round: int, parameters, config):
-
-        model = Net(num_classes, input_dim)
-
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-        params_dict = zip(model.state_dict().keys(), parameters)
-        state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
-        model.load_state_dict(state_dict, strict=True)
-
-        loss, accuarcy = test_nn(model, testloader, device)
-
-        return loss, {'accuarcy': accuarcy}
-    return evaluate_fn
+if __name__ == "__main__":
+    start_server()
