4391d3178f66a7c4e8b2ead03657c68d095b2054
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-07-05 16:05:07 -0400

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import flwr as fl
 
 def start_server():
     # Start Flower server
-    fl.server.start_server(config={"num_rounds": 3})
+    fl.server.start_server(server_address="0.0.0.0:8080", config={"num_rounds": 3})
 
 if __name__ == "__main__":
     start_server()
