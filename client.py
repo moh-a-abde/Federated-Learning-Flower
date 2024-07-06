e5d2f25796d62b42bb0d2227836acba9e3f859bc
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-07-06 18:23:02 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,33 +1,31 @@
-from collections import OrderedDict
 from typing import Dict, List, Tuple
 from flwr.common import NDArrays, Scalar
 import flwr as fl
 import model
+from torch.utils.data import DataLoader
 
 class FlowerClient(fl.client.NumPyClient):
-    def __init__(self, trainloader, valloader, testloader, dataset):
+    def __init__(self, trainloader: DataLoader, valloader: DataLoader, testloader: DataLoader):
         self.trainloader = trainloader
         self.valloader = valloader
         self.testloader = testloader
-        self.dataset = dataset
         self.model = None
 
     def fit(self, parameters: NDArrays, config: Dict[str, Scalar]) -> Tuple[NDArrays, int, Dict[str, Scalar]]:
-        self.model = model.train_xgboost(self.dataset)
+        self.model = model.train_xgboost(self.trainloader)
         return [], len(self.trainloader.dataset), {}
 
     def evaluate(self, parameters: NDArrays, config: Dict[str, Scalar]) -> Tuple[float, int, Dict[str, Scalar]]:
         accuracy = 0.99  # Placeholder for actual accuracy
         return 0.0, len(self.valloader.dataset), {'accuracy': accuracy}
 
-def generate_client_fn(trainloaders, valloaders, testloader, datasets):
+def generate_client_fn(trainloaders: List[DataLoader], valloaders: List[DataLoader], testloader: DataLoader):
     def client_fn(cid: str):
-        print(f"Creating client {cid}")
         cid_int = int(cid)
+        print(f"Creating client {cid_int}")
         if cid_int >= len(trainloaders):
             raise ValueError(f"Client ID {cid_int} is out of bounds for trainloaders of length {len(trainloaders)}")
         return FlowerClient(trainloader=trainloaders[cid_int],
                             valloader=valloaders[cid_int],
-                            testloader=testloader,
-                            dataset=datasets[cid_int])
+                            testloader=testloader)
     return client_fn
