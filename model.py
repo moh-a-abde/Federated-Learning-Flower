567191c8342284b95174889ed3ca1f77533e8c4b
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-07-06 18:50:13 -0400

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import xgboost as xgb
+from torch.utils.data import Dataset, DataLoader, random_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
