7a293abf4f04d674323e32b24bafe709dff8eca5
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-07-06 17:37:40 -0400

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -8,15 +8,12 @@ from typing import List
 
 class PreprocessedCSVDataset(Dataset):
     def __init__(self, csv_file, transform=None):
-        # Load and preprocess data
         self.data = pd.read_csv(csv_file)
         
-        # Define categorical and numerical features
         self.categorical_features = ['id.orig_h', 'id.resp_h', 'proto', 'history', 'uid', 'conn_state']
-        self.numerical_features = ['id.orig_p', 'orig_pkts',	'orig_ip_bytes',	'resp_pkts', 'missed_bytes'
-        , 'local_resp', 'local_orig', 'resp_bytes', 'orig_bytes', 'duration', 'id.resp_p']
+        self.numerical_features = ['id.orig_p', 'orig_pkts', 'orig_ip_bytes', 'resp_pkts', 'missed_bytes',
+                                   'local_resp', 'local_orig', 'resp_bytes', 'orig_bytes', 'duration', 'id.resp_p']
         
-        # Define the column transformer
         self.preprocessor = ColumnTransformer(
             transformers=[
                 ('num', StandardScaler(), self.numerical_features),
@@ -24,15 +21,13 @@ class PreprocessedCSVDataset(Dataset):
             ]
         )
         
-        # Separate features and labels
         self.features = self.data.drop(columns=['label', 'ts'])
         self.labels = self.data['label']
         self.label_encoder = LabelEncoder()
         self.labels_encoded = self.label_encoder.fit_transform(self.labels)
         
-        # Fit and transform the features
         self.features_transformed = self.preprocessor.fit_transform(self.features)
-        self.input_dim = self.features_transformed.shape[1]  # Define input_dim based on the transformed features' shape
+        self.input_dim = self.features_transformed.shape[1]
         self.transform = transform
 
     def __len__(self):
@@ -51,8 +46,7 @@ def get_csv_dataset(csv_file: str, transform=None):
     return dataset
 
 def prepare_dataset(num_partitions: int, batch_size: int, num_classes: int, val_ratio: float = 0.1, csv_files: List[str] = []):
-    trainloaders, valloaders, testloader = [], [], None
-    datasets = []
+    trainloaders, valloaders, datasets = [], [], []
     
     for csv_file in csv_files:
         dataset = get_csv_dataset(csv_file)
@@ -66,5 +60,7 @@ def prepare_dataset(num_partitions: int, batch_size: int, num_classes: int, val_
         
         trainloaders.append(DataLoader(for_train, batch_size=batch_size, shuffle=True, num_workers=2))
         valloaders.append(DataLoader(for_val, batch_size=batch_size, shuffle=False, num_workers=2))
+    
+    testloader = DataLoader(datasets[0], batch_size=64, shuffle=False, num_workers=2)  # Use one of the datasets for the test loader
 
     return trainloaders, valloaders, testloader, datasets
