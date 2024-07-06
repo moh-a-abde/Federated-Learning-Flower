24a6e5e6728127f380a93ad3459f9bb2cd988380
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-07-06 16:38:48 -0400

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -49,40 +49,21 @@ def get_csv_dataset(csv_file: str, transform=None):
     dataset = PreprocessedCSVDataset(csv_file, transform=transform)
     return dataset
 
-def prepare_dataset(num_partitions: int, batch_size: int, num_classes: int, val_ratio: float = 0.1, csv_file: str = 'data/raw/zeek_live_export_7012024a_final.csv'):
-    # Load and preprocess the dataset
-    dataset = get_csv_dataset(csv_file)
+def prepare_dataset(num_partitions: int, batch_size: int, num_classes: int, val_ratio: float = 0.1, csv_files: List[str] = []):
+    trainloaders, valloaders, testloader = [], [], None
+    datasets = []
     
-    # Check if dataset is loaded
-    print(f"Total number of samples: {len(dataset)}")
-
-    # Ensure no partition is empty by adjusting the partition lengths
-    num_images = len(dataset) // num_partitions
-    remainder = len(dataset) % num_partitions
-    partition_len = [num_images + 1 if i < remainder else num_images for i in range(num_partitions)]
-    
-    # Ensure that no partition length is zero
-    partition_len = [length for length in partition_len if length > 0]
-    
-    datasets = random_split(dataset, partition_len, torch.Generator().manual_seed(2024))
-
-    # Creating train and validation loaders
-    trainloaders = []
-    valloaders = []
-    for dataset_ in datasets:
-        num_total = len(dataset_)
+    for csv_file in csv_files:
+        dataset = get_csv_dataset(csv_file)
+        datasets.append(dataset)
+        
+        num_total = len(dataset)
         num_val = int(val_ratio * num_total)
         num_train = num_total - num_val
-
-        if num_train == 0:
-            continue  # Skip if no training data
         
-        for_train, for_val = random_split(dataset_, [num_train, num_val], torch.Generator().manual_seed(2024))
-
+        for_train, for_val = random_split(dataset, [num_train, num_val], torch.Generator().manual_seed(2024))
+        
         trainloaders.append(DataLoader(for_train, batch_size=batch_size, shuffle=True, num_workers=2))
         valloaders.append(DataLoader(for_val, batch_size=batch_size, shuffle=False, num_workers=2))
 
-    # Creating test loader
-    testloader = DataLoader(dataset, batch_size=64, shuffle=False, num_workers=2)
-    
-    return trainloaders, valloaders, testloader, dataset.input_dim
+    return trainloaders, valloaders, testloader, datasets
