67c94df25c031013032aa808d3c26dd65a9fb2f9
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-07-06 15:52:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,30 @@ import flwr as fl
 from client import generate_client_fn
 from dataset import prepare_dataset
 import pandas as pd
+import yaml
+
+def load_config(config_file):
+    with open(config_file, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
 
 def main():
+    # Load configuration
+    config = load_config('base.yaml')
+
+    # Extract parameters from configuration
+    num_partitions = config['num_clients']
+    batch_size = config['batch_size']
+    num_classes = config['num_classes']
+    val_ratio = config.get('val_ratio', 0.1)  # Default to 0.1 if not specified
+    csv_file = config['data']['file_path']
+    num_rounds = config['num_rounds']
+
     # Prepare dataset
-    num_partitions = 10
-    batch_size = 64
-    num_classes = 5  
-    val_ratio = 0.1
-    csv_file = 'data/raw/zeek_live_export_7012024a_final.csv'
-    
-    trainloaders, valloaders, testloader, input_dim = prepare_dataset(num_partitions, batch_size, num_classes, val_ratio, csv_file)
-    
+    trainloaders, valloaders, testloader, input_dim = prepare_dataset(
+        num_partitions, batch_size, num_classes, val_ratio, csv_file
+    )
+
     # Load the full dataset to pass to the XGBoost function
     dataset = pd.read_csv(csv_file)
 
@@ -21,7 +34,7 @@ def main():
     fl.simulation.start_simulation(
         client_fn=client_fn,
         num_clients=num_partitions,
-        config=fl.server.ServerConfig(num_rounds=10),
+        config=fl.server.ServerConfig(num_rounds=num_rounds),
     )
 
 if __name__ == "__main__":
