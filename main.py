6c2faf322b7668a6c2658459849ce81f3693b69b
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-07-06 16:39:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,26 +10,20 @@ def load_config(config_file):
     return config
 
 def main():
-    # Load configuration
     config = load_config('conf/base.yaml')
 
-    # Extract parameters from configuration
     num_partitions = config['num_clients']
     batch_size = config['batch_size']
     num_classes = config['num_classes']
-    val_ratio = config.get('val_ratio', 0.1)  # Default to 0.1 if not specified
-    csv_file = config['data']['file_path']
+    val_ratio = config.get('val_ratio', 0.1)
+    csv_files = [config['data'][f'file_path_{i}'] for i in range(1, 6)]
     num_rounds = config['num_rounds']
 
-    # Prepare dataset
-    trainloaders, valloaders, testloader, input_dim = prepare_dataset(
-        num_partitions, batch_size, num_classes, val_ratio, csv_file
+    trainloaders, valloaders, testloader, datasets = prepare_dataset(
+        num_partitions, batch_size, num_classes, val_ratio, csv_files
     )
 
-    # Load the full dataset to pass to the XGBoost function
-    dataset = pd.read_csv(csv_file)
-
-    client_fn = generate_client_fn(trainloaders, valloaders, testloader, dataset)
+    client_fn = generate_client_fn(trainloaders, valloaders, testloader, datasets)
 
     fl.simulation.start_simulation(
         client_fn=client_fn,
