022e40f620965d0440783bacac2786b22946cb69
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-07-06 17:37:59 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
 from collections import OrderedDict
-from typing import Dict, List
+from typing import Dict, List, Tuple
 from flwr.common import NDArrays, Scalar
-from typing import Tuple
 import flwr as fl
 import model
 
@@ -21,10 +20,14 @@ class FlowerClient(fl.client.NumPyClient):
         accuracy = 0.99  # Placeholder for actual accuracy
         return 0.0, len(self.valloader.dataset), {'accuracy': accuracy}
 
-def generate_client_fn(trainloaders, valloaders, testloader, dataset):
+def generate_client_fn(trainloaders, valloaders, testloader, datasets):
     def client_fn(cid: str):
-        return FlowerClient(trainloader=trainloaders[int(cid)],
-                            valloader=valloaders[int(cid)],
+        print(f"Creating client {cid}")
+        cid_int = int(cid)
+        if cid_int >= len(trainloaders):
+            raise ValueError(f"Client ID {cid_int} is out of bounds for trainloaders of length {len(trainloaders)}")
+        return FlowerClient(trainloader=trainloaders[cid_int],
+                            valloader=valloaders[cid_int],
                             testloader=testloader,
-                            dataset=dataset)
+                            dataset=datasets[cid_int])
     return client_fn
