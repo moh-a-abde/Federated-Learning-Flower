1fa6f38c81788558d097654e568dc8e88a218f5d
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-07-06 17:21:09 -0400

    Update model.py

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
 import time
+from typing import List
 
 def train_xgboost(data):
     # Encode categorical features
