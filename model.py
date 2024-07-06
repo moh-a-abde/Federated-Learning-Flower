41be674d373f4d2892e1aafa91172ca8523a0973
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-07-06 18:46:07 -0400

    Update model.py

--- model.py
+++ model.py
@@ -3,39 +3,41 @@ import xgboost as xgb
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
-import time
-from typing import List
 
-def train_xgboost(data):
-    # Encode categorical features
-    label_encoder = LabelEncoder()
-    # Print unique values in the 'label' column
-    unique_labels = data['label'].unique()
-    print("Unique values in 'label' column:", unique_labels)
-    data['label'] = label_encoder.fit_transform(data['label'])
+def train_xgboost(trainloader: DataLoader):
 
-    # Separate the 'ts' column into a different dataset
-    ts_data = data[['ts']]
+    # Combine data from DataLoader into a DataFrame
+    data_list = []
+    for features, labels in trainloader:
+        features_np = features.numpy()
+        labels_np = labels.numpy()
+        data_list.append((features_np, labels_np))
+
+    X = np.concatenate([x[0] for x in data_list])
+    y = np.concatenate([x[1] for x in data_list])
+
+    #label_encoder = LabelEncoder()
 
     # Select features and target
-    X = data.drop(columns=['label', 'ts', 'uid'])
-    y = data['label']
+    #X = data.drop(columns=['label', 'ts', 'uid'])
 
     # Convert categorical features to numerical
-    X = pd.get_dummies(X, columns=['id.orig_h', 'id.resp_h', 'proto', 'conn_state', 'history'])
+    #X = pd.get_dummies(X, columns=['id.orig_h', 'id.resp_h', 'proto', 'conn_state', 'history'])
 
     # Standardize the features
-    scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
+    #scaler = StandardScaler()
+    #X_scaled = scaler.fit_transform(X)
 
     # Set the test size
     tsz = 0.30
 
-    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=tsz, stratify=y, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=tsz, stratify=y, random_state=42)
+    
     l = len(set(y))
-    startTrainTime = time.time()
+    
     train = xgb.DMatrix(X_train, label=y_train)
     test = xgb.DMatrix(X_test, label=y_test)
+    
     param = {
         'max_depth': 6,
         'eta': 0.35,
@@ -56,16 +58,12 @@ def train_xgboost(data):
     print(cv_results)
     best_num_boost_round = cv_results.shape[0]
     model = xgb.train(param, train, num_boost_round=best_num_boost_round)
-    TrainTime = (time.time() - startTrainTime)
-    startTestTime = time.time()
-    predictions = model.predict(train)
-    TestTime = (time.time() - startTestTime)
-    print(f"Training Time: {TrainTime} seconds")
-    print(f"Testing Time: {TestTime} seconds")
-    accuracy = accuracy_score(y_train, predictions)
-    report = classification_report(y_train, predictions)
+
+    predictions = model.predict(test)
+    accuracy = accuracy_score(test, predictions)
+    report = classification_report(test, predictions)
     print('XGBoost Model Training Metrics:')
-    print(f'Accuracy: {accuracy}')
+    print('Accuracy: {accuracy}')
     print('Classification Report:')
     print(report)
     return model
