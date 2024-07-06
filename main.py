163e46ad8fde217c16ca26ba1436c54bb1be8f96
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-07-06 17:37:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from client import generate_client_fn
 from dataset import prepare_dataset
 import pandas as pd
 import yaml
-from typing import List
 
 def load_config(config_file):
     with open(config_file, 'r') as file:
@@ -19,6 +18,9 @@ def main():
     val_ratio = config.get('val_ratio', 0.1)
     csv_files = [config['data'][f'file_path_{i}'] for i in range(1, 6)]
     num_rounds = config['num_rounds']
+
+    if len(csv_files) != num_partitions:
+        raise ValueError(f"Number of CSV files ({len(csv_files)}) does not match number of clients ({num_partitions})")
 
     trainloaders, valloaders, testloader, datasets = prepare_dataset(
         num_partitions, batch_size, num_classes, val_ratio, csv_files
