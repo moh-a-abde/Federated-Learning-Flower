8af35d8da126e9e097c6ba35023433a8831d58dc
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-07-07 19:54:06 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,11 @@
+from collections import OrderedDict
 from typing import Dict, List, Any
 from flwr.common import NDArrays, Scalar
 import flwr as fl
 import model
 from torch.utils.data import DataLoader
 import ray
+from model import train_xgboost
 
 class FlowerClient(fl.client.NumPyClient):
     def __init__(self, trainloader: DataLoader, valloader: DataLoader, testloader: DataLoader):
@@ -12,23 +14,36 @@ class FlowerClient(fl.client.NumPyClient):
         self.testloader = testloader
         self.model = None
 
-    def fit(self, parameters: NDArrays, config: Dict[str, Scalar]) -> Dict[str, Any]:
+    def set_parameters(self, parameters):
+        param_dict = {k: v for k, v in zip(self.model.feature_names, parameters)}
+        self.model.set_attr(**param_dict)
+
+    def get_parameters(self, config: Dict[str, Scalar]):
+        return [self.model.attr(name) for name in self.model.feature_names]
+
+    def fit(self, parameters, config):
+        # copy parameters sent by the server into client's local model
+        self.set_parameters(parameters)
+
         self.model = model.train_xgboost(self.trainloader)
+        # do local training
+        train_xgboost(self.trainloader)
         return {
             "parameters": [],
             "num_examples": len(self.trainloader.dataset),
             "metrics": {}
         }
 
-    def evaluate(self, parameters: NDArrays, config: Dict[str, Scalar]) -> Dict[str, Any]:
+    def evaluate(self, parameters: NDArrays, config: Dict[str, Scalar]):
         accuracy = 0.99  # Placeholder for actual accuracy
+        self.set_parameters(parameters)
         return {
             "loss": 0.0,
             "num_examples": len(self.valloader.dataset),
             "metrics": {'accuracy': accuracy}
         }
 
-def generate_client_fn(trainloaders: List[DataLoader], valloaders: List[DataLoader], testloader: DataLoader):
+def generate_client_fn(trainloaders, valloaders, testloader):
     def client_fn(cid: str):
         cid_int = int(cid)
         print(f"Creating client {cid_int}")
