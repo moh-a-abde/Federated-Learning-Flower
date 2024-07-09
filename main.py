7c2b48350d8a4c9123862cab5c234912c0581710
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-07-09 14:09:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -29,13 +29,13 @@ def main():
 
     client_fn = generate_client_fn(trainloaders, valloaders, datasets[0])
 
-    ray.init()
+    ray.init(num_cpus=2)
 
     history = fl.simulation.start_simulation(
         client_fn=client_fn,
         num_clients=num_partitions,
         config=fl.server.ServerConfig(num_rounds=num_rounds),
-        client_resources={'num_cpus': 2},
+        client_resources={'num_cpus': 1},
     )
 
 if __name__ == "__main__":
