b5b0289f1b0f792f880a7dc9ae4680bd822d8446
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-07-10 15:10:14 -0400

    Update server.py

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ def get_on_fit_config():
 def get_evaluate_fn():
     def evaluate_fn(server_round: int, parameters, config):
         model = train_xgboost()
-        accuracy = 0.99  # Placeholder for actual accuracy
+        accuracy = 0.00  # Placeholder for actual accuracy
         return 0.0, {'accuracy': accuracy}
     return evaluate_fn
 
@@ -18,13 +18,13 @@ def start_server():
     strategy = fl.server.strategy.FedAvg(
         fraction_fit=0.1,
         fraction_eval=0.1,
-        min_fit_clients=2,
-        min_eval_clients=2,
-        min_available_clients=2,
+        min_fit_clients=1,
+        min_eval_clients=1,
+        min_available_clients=1,
         eval_fn=get_evaluate_fn(),
         on_fit_config_fn=get_on_fit_config(),
     )
-    fl.server.start_server(config={"num_rounds": 10}, strategy=strategy)
+    fl.server.start_server(config, strategy=strategy)
 
 if __name__ == "__main__":
     start_server()
